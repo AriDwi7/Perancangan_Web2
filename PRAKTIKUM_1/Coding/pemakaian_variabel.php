71fbd9e455d8cef0fcf58ea3ccfd564e1edb0c2a
AriDwi7 <dwia72583@gmail.com>
2025-11-25 11:06:22 +0700

    Add PHP examples for basic control structures
    Added multiple PHP files demonstrating fundamental programming concepts: variable usage, arithmetic operations, assignment, comparison, logical operators, and control structures (if, if-else, switch, for, while, do-while, break, continue). Also included a 'tugas.php' file with number pattern exercises. These files serve as practical examples for learning PHP syntax and logic.

---
+++ PRAKTIKUM_1/Coding/pemakaian_variabel.php
@@ -0,0 +1,12 @@
+
+<html>
+<head>
+    <title> Contoh Skrip PHP </title>
+</head>
+<body>
+    <?php
+    $nama = "Diva Oktaviana";
+    echo "$nama berkata \"Katakan namamu!\"";
+    ?>
+</body>
+</html>
